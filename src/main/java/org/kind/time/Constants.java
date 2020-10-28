f1d56fa75956ff47e39b6f0e413b41bf6b540871
starkleem <leem@starktechgroup.com>
2020-10-28 15:31:27 -0400

    first commit

---
+++ src/main/java/org/kind/time/Constants.java
@@ -0,0 +1,8 @@
+package org.kind.time;
+
+public final class Constants {
+    static final int MILLISECONDS_IN_SECOND = 1000;
+    static final int MILLISECONDS_IN_MINUTE = 60000;
+    static final int MILLISECONDS_IN_HOUR = 3600000;
+    static final int MILLISECONDS_IN_DAY = 86400000;
+}
---
+++ src/main/java/org/kind/time/TimeDiff.java
@@ -0,0 +1,47 @@
+package org.kind.time;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class TimeDiff {
+
+    public int seconds(Date startDate, Date endDate) {
+        final long diff = endDate.getTime() - startDate.getTime();
+        return (int)(diff / Constants.MILLISECONDS_IN_SECOND);
+    }
+
+    public int minutes(Date startDate, Date endDate) {
+        final long diff = endDate.getTime() - startDate.getTime();
+        return (int)(diff / Constants.MILLISECONDS_IN_MINUTE);
+    }
+
+    public int hours(Date startDate, Date endDate) {
+        final long diff = endDate.getTime() - startDate.getTime();
+        return (int)(diff / Constants.MILLISECONDS_IN_HOUR);
+    }
+
+    public int days(Date startDate, Date endDate) {
+        final Calendar c1 = new GregorianCalendar();
+        c1.setTime(startDate);
+        final Calendar c2 = new GregorianCalendar();
+        c2.setTime(endDate);
+        return days((Calendar)c1, (Calendar)c2);
+    }
+
+    public int days(Calendar startDate, Calendar endDate) {
+        long diff = endDate.getTimeInMillis() - startDate.getTimeInMillis();
+        int offset = endDate.get(16) - startDate.get(16);
+        diff += (long)offset;
+        int days = (int)(diff / Constants.MILLISECONDS_IN_DAY);
+        return Math.abs(days);
+    }
+
+    public int hours(Calendar startDate, Calendar endDate) {
+        long diff = endDate.getTimeInMillis() - startDate.getTimeInMillis();
+        int offset = endDate.get(16) - startDate.get(16);
+        diff += (long)offset;
+        int hours = (int)(diff / Constants.MILLISECONDS_IN_HOUR);
+        return hours;
+    }
+}
